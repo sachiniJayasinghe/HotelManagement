4cc105fe25cc7458dbcc5a2ed5001fe565046277
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-09-26 19:43:51 +0530

    Creating Util for Parsing & Validating JWT

--- src/main/java/com/example/HotelServer/util/JwtUtil.java
+++ src/main/java/com/example/HotelServer/util/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.example.HotelServer.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -20,6 +23,35 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
                 .signWith(SignatureAlgorithm.HS256, getSigningKey()).compact();
     }
+    public  String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    public boolean isTokenValid(String token , UserDetails userDetails){
+        final String userName  = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private  Claims extractAllClaims(String token){
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    public  String extractUserName(String token){
+        return extractClaims(token,Claims::getSubject);
+
+    }
+    private  Date extractExpiration(String token){
+        return extractClaims(token,Claims::getExpiration);
+
+    }
+    private  boolean  isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+    private <T> T extractClaims(String token , Function<Claims ,T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
     private Key getSigningKey(){
         byte[] keyBytes = Decoders.BASE64.decode("413F4428472B4B6250655368566D5970337336763979244226452948484D6351");
         return Keys.hmacShaKeyFor(keyBytes);
